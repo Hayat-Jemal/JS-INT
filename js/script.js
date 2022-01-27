219619331ec89d56a393d9b4045d68322a295f20
Hayat Jemal <htjemal2000@gmail.com>
2022-01-27 23:50:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//constructor function
+function Node(value,next,prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+let node1 = new Node(100,"node2", null)
+console.log(node1)
+
+
+
