203d125ccdec2ae48f4bbf3cbb076530ac373669
Hayat Jemal <htjemal2000@gmail.com>
2022-02-10 08:04:29 +0300

    add Q

--- js/script.js
+++ js/script.js
@@ -175,3 +175,7 @@ console.log(isMagicArray([21, 3, 7, 9, 11, 4, 6]));
 console.log(isMagicArray([13, 4, 4, 4, 4]));
 console.log(isMagicArray([0, 6, 8, 20]));
 console.log(isMagicArray([3]));
+
+ // 7. Wave array 
+// // //   A wave array is defined to an array which does not contain two even numbers or two odd numbers in adjacent locations. So {7, 2, 9, 10, 5}, {4, 11, 12, 1, 6}, {1, 0, 5} and {2} are all wave arrays. But {2, 6, 3, 4} is not a wave array because the even numbers 2 and 6 are adjacent to each other.
+// // //   Write a function named isWave that returns 1 if its array argument is a Wave array, otherwise it returns 0.
