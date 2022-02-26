11eeddeb18c7058e608ba31dc1788eddeb12632f
Hayat Jemal <htjemal2000@gmail.com>
2022-02-27 01:14:40 +0300

    other solution

--- js/script.js
+++ js/script.js
@@ -411,3 +411,16 @@ console.log(w)
 //   ])
 //  );
 
+// ******************************************
+// // // Largest Swap
+// // // Write a function that takes a two-digit number and determines if it's the largest of two possible digit swaps.
+// // // largestSwap(27) ➞ false
+// // // largestSwap(43) ➞ true
+// // // If 27 is our input, we should return false because swapping the digits gives us 72, and 72 > 27. On the other hand, swapping 43 gives us 34, and 43 > 34.
+// function largestSwap(num) {
+//  return num.toString().charAt(0) >= num.toString().charAt(1);
+//  }
+// console.log(largestSwap(14));
+// // largestSwap(14) ➞ false
+// console.log(largestSwap(54));
+
