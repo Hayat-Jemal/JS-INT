7c93b880f15d63d51559355c8420badbd5c9fb7d
Hayat Jemal <htjemal2000@gmail.com>
2022-03-06 00:25:44 +0300

    leap year

--- js/script.js
+++ js/script.js
@@ -550,4 +550,20 @@ var a = result.sort(function (a, b) {
   return a - b;
 })
 // console.log(a)
+// * How to determine whether a year is a leap year
+// To determine whether a year is a leap year, follow these steps:
+// 1) If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+// 2) If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+// 3) If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+// 4) The year is a leap year (it has 366 days).
+// 5) The year is not a leap year (it has 365 days)
+// */
+// **method 1**
+// function leapYear(year){
+//     if((year % 4 == 0) && (year % 100 != 0 || year % 400 == 0))        
+//         return "leap year";
+//         else
+//         return "not leap year";
+// }
+// console.log(leapYear(2486));
 
