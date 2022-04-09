e15d7686f27a5340c159c9e746aea3d33a115d23
Hayat Jemal <htjemal2000@gmail.com>
2022-04-10 00:49:45 +0300

    changeToBinary

--- js/script.js
+++ js/script.js
@@ -723,3 +723,21 @@ function whichIsLarger(f, g) {
 // console.log(whichIsLarger(5, 10)); //g
 // console.log(whichIsLarger(25,25)); //neither
 
+// ********************************************
+// -Convert a Number to Base - 2
+// -Create a function that returns a base-2 (binary) representation of a base-10 (decimal) string number. To convert is simple: ((2) means base-2 and (10) means base-10) 010101001(2) = 1 + 8 + 32 + 128.
+// Going from right to left, the value of the most right bit is 1, now from that every bit to the left will be x2 the value, value of an 8 bit binary numbers are (256, 128, 64, 32, 16, 8, 4, 2, 1).
+function changeToBinary(n) {
+  let binary = " ";
+  while (n > 0) {
+    binary = n % 2 + binary;
+          // =1+0+1
+    n = Math.floor(n / 2);
+  }
+  return binary;
+}
+console.log(changeToBinary(5));//1= 1*1 = 1
+console.log(changeToBinary(4));//101=1*1 + 1*4 = 5
+console.log(changeToBinary(10));//1010= 1*2 + 1*8 = 10
+// 1*2 + 1*8 = 10
+
